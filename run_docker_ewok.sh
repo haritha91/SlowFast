80e06d204491443af576ad376112100a651a3591
Haritha Thilakarathne <harithalht@gmail.com>
2023-02-09 23:47:49 +1100

    change volume mounts

--- run_docker_ewok.sh
+++ run_docker_ewok.sh
@@ -1,10 +1,10 @@
 # build docker image
-docker build -f Dockerfile --rm -t docker_slowfast .
+docker build -f Dockerfile --rm -t docker_slowfast_nb .
 
 #run on local with docker
 docker run --gpus all \
 --shm-size=8g -it \
 --ipc="host" \
---volume=/home/haritha/datasets/Kinetics-400-resized:/app/data \
---volume=/home/haritha/results/slowfast:/app/results \
-docker_slowfast
+--volume=/home/haritha/datasets/Kinetics-Netball:/app/data \
+--volume=/home/haritha/results/slowfast-netball:/app/results \
+docker_slowfast_nb
