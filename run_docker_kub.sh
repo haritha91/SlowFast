b5725c29c86a94317d170f0af0cf9232e7b752e2
Haritha Thilakarathne <harithalht@gmail.com>
2023-02-13 00:20:06 +1100

    add docker run

---
+++ run_docker_kub.sh
@@ -0,0 +1,10 @@
+#/usr/bin/env bash
+
+IMAGE="registry.dl.cs.latrobe.edu.au/haritha/sfnthree"
+
+# 1. Build image
+docker build . -t "$IMAGE"
+# 2. Push image to cluster
+docker push "$IMAGE"
+# 3. Run container on cluster
+kubectl apply -f pod.yml
