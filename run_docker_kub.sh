1effa8a6c00c11e9baba743fd9a2e4d192af247c
Haritha Thilakarathne <harithalht@gmail.com>
2023-02-27 11:22:55 +1100

    change for L exp

--- run_docker_kub.sh
+++ run_docker_kub.sh
@@ -1,6 +1,6 @@
 #/usr/bin/env bash
 
-IMAGE="registry.dl.cs.latrobe.edu.au/haritha/kinetics-test-2"
+IMAGE="registry.dl.cs.latrobe.edu.au/haritha/kinetics-test-L-1"
 
 # 1. Build image
 docker build . -t "$IMAGE"
